9d89e64786f5f19c04adb60db2f6c67a5ea07c4a
petriwna <petriwna43@gmail.com>
2024-02-06 13:45:52 +0200

    add center container toolbar

--- src/app/Component/ToolbarComponent/toolbar/toolbar.component.ts
+++ src/app/Component/ToolbarComponent/toolbar/toolbar.component.ts
@@ -1,15 +1,15 @@
 import { Component } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
+import { CenterContainerToolbarComponent } from "../center-container-toolbar/center-container-toolbar.component";
 import { LeftContainerToolbarComponent } from "../left-container-toolbar/left-container-toolbar.component";
 
 @Component({
     selector: "app-toolbar",
     standalone: true,
-    imports: [MatToolbarModule, LeftContainerToolbarComponent],
+    imports: [MatToolbarModule, LeftContainerToolbarComponent, CenterContainerToolbarComponent],
     templateUrl: "./toolbar.component.html",
     styleUrl: "./toolbar.component.scss"
 })
 export class ToolbarComponent {
-
 }
--- src/app/Component/app.component.ts
+++ src/app/Component/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
-import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { ToolbarComponent } from "./ToolbarComponent/toolbar/toolbar.component";
 
 @Component({
     selector: "app-root",
